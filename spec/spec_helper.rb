89034cbc50497b0f8ae812303deb3a396311b1c6
yosi <todacchi.desu@gmail.com>
2022-03-13 02:35:45 +0900

    rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
+# require 'one_time_password'
+
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require_relative "../test/dummy/config/environment"
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
+require_relative "../spec/dummy/config/environment"
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../spec/dummy/db/migrate", __dir__)]
 require "rails/test_help"
 
 # Load fixtures from the engine
