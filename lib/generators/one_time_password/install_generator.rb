1c80c07d0e35cff2aa710e45375c6f51e01ebea7
yosi <todacchi.desu@gmail.com>
2022-03-20 23:44:56 +0900

    Resolve error: next_migration_number, uninitialized constant ActiveRecord::Generators (NameError)

--- lib/generators/one_time_password/install_generator.rb
+++ lib/generators/one_time_password/install_generator.rb
@@ -1,6 +1,13 @@
+require "rails/generators"
+require "rails/generators/active_record"
+
 module OneTimePassword
   class InstallGenerator < ::Rails::Generators::Base
     include ::Rails::Generators::Migration
+
+    def self.next_migration_number(dirname)
+      ::ActiveRecord::Generators::Base.next_migration_number(dirname)
+    end
 
     source_root File.expand_path('templates', __dir__)
 
