1135c0f71995c682f3178f354489685cba7aee8b
yosi <todacchi.desu@gmail.com>
2022-03-21 09:26:59 +0900

    add warning_over_write option(rails g one_time_password:install --warning_over_write)

--- lib/generators/one_time_password/install_generator.rb
+++ lib/generators/one_time_password/install_generator.rb
@@ -5,6 +5,9 @@ module OneTimePassword
   class InstallGenerator < ::Rails::Generators::Base
     include ::Rails::Generators::Migration
 
+    class_option :warning_over_write, type: :boolean, default: false,
+      desc: "Orver write generator files."
+
     def self.next_migration_number(dirname)
       ::ActiveRecord::Generators::Base.next_migration_number(dirname)
     end
@@ -14,10 +17,10 @@ module OneTimePassword
     def create_initializer_file
       template = 'one_time_password'
       file_name =  "config/initializers/#{template}.rb"
-      if File.exist?(file_name)
-        ::Kernel.warn "Model already exists: #{template}"
-      else
+      if !File.exist?(file_name) || options[:warning_over_write]
         template("#{template}.rb", file_name)
+      else
+        ::Kernel.warn "Model already exists: #{template}"
       end
     end
 
@@ -25,24 +28,24 @@ module OneTimePassword
       template = 'create_one_time_authentication'
       migration_dir = File.expand_path("db/migrate")
 
-      if self.class.migration_exists?(migration_dir, template)
-        ::Kernel.warn "Migration already exists: #{template}"
-      else
+      if !self.class.migration_exists?(migration_dir, template) || options[:warning_over_write]
         migration_template(
             "#{template}.rb.erb",
             "#{migration_dir}/#{template}.rb",
             migration_version: migration_version
         )
+      else
+        ::Kernel.warn "Migration already exists: #{template}"
       end
     end
 
     def create_model_file
       template = 'one_time_authentication'
       file_name =  "app/models/#{template}.rb"
-      if File.exist?(file_name)
-        ::Kernel.warn "Model already exists: #{template}"
-      else
+      if !File.exist?(file_name) || options[:warning_over_write]
         template("#{template}.rb", file_name)
+      else
+        ::Kernel.warn "Model already exists: #{template}"
       end
     end
 
