09791fe37071852e037f98f7fcfb4814cef63e41
yosi <todacchi.desu@gmail.com>
2022-03-25 03:47:15 +0900

    add scope: recent OneTimeAuthentication

--- lib/generators/one_time_password/templates/app/models/one_time_authentication.rb
+++ lib/generators/one_time_password/templates/app/models/one_time_authentication.rb
@@ -4,4 +4,8 @@ class OneTimeAuthentication < OneTimePassword::Models::OneTimeAuthentication
   scope :unauthenticated, -> {
     where(authenticated_at: nil)
   }
+
+  scope :recent, -> (time_ago) {
+    order(created_at: :desc).where(created_at: ..Time.zone.now.ago(time_ago))
+  }
 end
