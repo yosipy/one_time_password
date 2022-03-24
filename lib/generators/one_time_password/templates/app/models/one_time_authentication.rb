558b5b346c7d14d76205919633f992d597513993
yosi <todacchi.desu@gmail.com>
2022-03-25 03:46:22 +0900

    add scope: authenticated_at is nil

--- lib/generators/one_time_password/templates/app/models/one_time_authentication.rb
+++ lib/generators/one_time_password/templates/app/models/one_time_authentication.rb
@@ -1,3 +1,7 @@
 class OneTimeAuthentication < OneTimePassword::Models::OneTimeAuthentication
   enum function_name: OneTimePassword::FUNCTION_NAMES
+
+  scope :unauthenticated, -> {
+    where(authenticated_at: nil)
+  }
 end
