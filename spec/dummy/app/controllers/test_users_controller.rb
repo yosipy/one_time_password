ace68c418292e0d39b84ccb142a3928a31430d7a
yosi <todacchi.desu@gmail.com>
2022-03-29 01:07:27 +0900

    temp

--- spec/dummy/app/controllers/test_users_controller.rb
+++ spec/dummy/app/controllers/test_users_controller.rb
@@ -1,4 +1,28 @@
 class TestUsersController < ApplicationController
   def new
   end
+
+  def create_auth
+    if params[:email].present?
+      auth = OneTimePassword::Auth.new(
+        OneTimePassword::FUNCTION_NAMES[:sign_up],
+        0,
+        params[:email]
+      )
+      one_time_authentication = auth.create_one_time_authentication
+    else
+      # error
+    end
+  end
+
+  def create
+    if params[:email].present?
+      auth = OneTimePassword::Auth.new(
+        OneTimePassword::FUNCTION_NAMES[:sign_up],
+        0,
+        params[:email]
+      )
+      one_time_authentication = auth.find_one_time_authentication
+    end
+  end
 end
