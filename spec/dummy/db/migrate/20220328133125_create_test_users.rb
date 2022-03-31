dff315652cd47a3d3018560f1645ec465a76ddf3
yosi <todacchi.desu@gmail.com>
2022-03-31 11:23:20 +0900

    migration create_table test_user

--- spec/dummy/db/migrate/20220328133125_create_test_users.rb
+++ spec/dummy/db/migrate/20220328133125_create_test_users.rb
@@ -1,6 +1,8 @@
 class CreateTestUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :test_users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
 
       t.timestamps
     end
